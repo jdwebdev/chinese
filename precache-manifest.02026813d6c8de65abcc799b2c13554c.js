1d961eb907ddadbe0984630b8a6295d1d76f2113
jd <jonathand@keyweo.com>
2025-03-13 19:19:22 +0100

    Updates

---
+++ precache-manifest.02026813d6c8de65abcc799b2c13554c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4591336c41072d0588bc0dd6c35b9474",
+    "url": "/jadonaword/index.html"
+  },
+  {
+    "revision": "221433d333be54848dba",
+    "url": "/jadonaword/static/css/main.9e5fea64.chunk.css"
+  },
+  {
+    "revision": "c3334bd6ad8931762805",
+    "url": "/jadonaword/static/js/2.b09d2f7a.chunk.js"
+  },
+  {
+    "revision": "eb045057ff83719bad78a45c9ced3bbf",
+    "url": "/jadonaword/static/js/2.b09d2f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221433d333be54848dba",
+    "url": "/jadonaword/static/js/main.455f62eb.chunk.js"
+  },
+  {
+    "revision": "4b7afcb717fee2040b6e",
+    "url": "/jadonaword/static/js/runtime-main.c34dbc8b.js"
+  }
+]);
